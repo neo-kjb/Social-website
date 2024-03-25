5a10f37a5bc0a14f07683ce6e7a4f593afdb2b96
neo-kjb <115832171+neo-kjb@users.noreply.github.com>
2024-03-25 03:55:12 +0300

    Creating images

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -6,7 +6,7 @@ from django.utils.text import slugify
 class Image(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        related_name="images_cretaed",
+        related_name="images_created",
         on_delete=models.CASCADE,
     )
     title = models.CharField(max_length=200)
