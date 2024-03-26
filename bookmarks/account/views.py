19d39bcfe0fdfc639aa8a3699c4856d294d9bc14
neo-kjb <115832171+neo-kjb@users.noreply.github.com>
2024-03-26 22:39:05 +0300

    Adding user follow/unfollow actions

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from .models import Contact
 
 # def user_login(request):
 #     if request.method == "POST":
@@ -84,3 +87,22 @@ def user_detail(request, username):
     return render(
         request, "account/user/detail.html", {"section": "people", "user": user}
     )
+
+
+@login_required
+@require_POST
+def user_follow(request):
+    user_id = request.POST.get("id")
+    action = request.POST.get("action")
+
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == "follow":
+                Contact.objects.get_or_create(user_from=request.user, user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({"status": "ok"})
+        except user.DoesNotExist:
+            return JsonResponse({"status": "error"})
+    return JsonResponse({"status": "error"})
