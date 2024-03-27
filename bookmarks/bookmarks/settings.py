b7ebc7b0f7078b65d9f7857f4e2e872b3d1bab75
neo-kjb <115832171+neo-kjb@users.noreply.github.com>
2024-03-28 02:43:30 +0300

    Adding Django Debug Toolbar

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -48,9 +48,11 @@ INSTALLED_APPS = [
     "images",
     "easy_thumbnails",
     "actions",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -178,3 +180,14 @@ if DEBUG:
 ABSOLUTE_URL_OVERRIDES = {
     "auth.user": lambda u: reverse_lazy("user_detail", args=[u.username])
 }
+
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+if DEBUG:
+    import mimetypes
+
+    mimetypes.add_type("application/javascript", ".js", True)
+    mimetypes.add_type("text/css", ".css", True)
