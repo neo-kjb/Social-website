7f51a8b68f717e0dda8cceca5348e522288c6829
neo-kjb <115832171+neo-kjb@users.noreply.github.com>
2024-03-28 03:22:49 +0300

    Count image views using Redis

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -7,6 +7,12 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from actions.utils import create_action
+import redis
+from django.conf import settings
+
+r = redis.Redis(
+    host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
+)
 
 
 @login_required
@@ -30,8 +36,11 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f"image{image.id}:views")
     return render(
-        request, "images/image/detail.html", {"section": "images", "image": image}
+        request,
+        "images/image/detail.html",
+        {"section": "images", "image": image, "total_views": total_views},
     )
 
 
